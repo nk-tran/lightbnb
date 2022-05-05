a040debe35b01c75f236eeb1a49baa0c89bc196e
Nhan <nktran4@gmail.com>
2022-05-05 19:17:24 -0400

    reservation query add

--- LightBnB_WebApp/server/database.js
+++ LightBnB_WebApp/server/database.js
@@ -98,9 +98,32 @@ exports.addUser = addUser;
  * @param {string} guest_id The id of the user.
  * @return {Promise<[{}]>} A promise to the reservations.
  */
-const getAllReservations = function(guest_id, limit = 10) {
-  return getAllProperties(null, 2);
-};
+// const getAllReservations = function(guest_id, limit = 10) {
+//   return getAllProperties(null, 2);
+// };
+
+const getAllReservations = function (guest_id, limit = 10) {
+  const query = `SELECT reservations.id, properties.title, properties.cost_per_night, reservations.start_date, avg(rating) as average_rating
+  FROM reservations
+  JOIN properties ON reservations.property_id = properties.id
+  JOIN property_reviews ON properties.id = property_reviews.property_id
+  WHERE reservations.guest_id = $1
+  GROUP BY properties.id, reservations.id
+  ORDER BY reservations.start_date
+  LIMIT $2
+  RETURNING *;`;
+  const values = [guest_id, limit];
+
+  return pool.query(query, values)
+    .then((result) => {
+      console.log(result.rows);
+      return result.rows;
+    })
+    .catch((e) => {
+      console.log(e);
+      return null;
+    });
+  }
 exports.getAllReservations = getAllReservations;
 
 /// Properties
